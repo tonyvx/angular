3ff0a022953ac4dc8b4b4fb5e2363864d990acc7
Tony <tonyvx@tony.home>
2018-03-08 19:09:11 -0500

    updated node app ref

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,9 +2,9 @@
 import { Injectable } from '@angular/core';
 @Injectable()
 export class Configuration {
-  public Server = 'http://127.0.0.1:8081/';
-  public ApiUrl = 'api/databases/goals/goal';
-  public PostApiUrl = 'postapi/databases/goals/goal';
+  public Server = 'http://127.0.0.1:8082/';
+  public ApiUrl = 'api/databases/local/goal';
+  public PostApiUrl = 'postapi/databases/local/goal';
   public ServerWithApiUrl = this.Server + this.ApiUrl;
 
   getConfig() {
